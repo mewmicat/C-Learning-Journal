0929c6101d8bd462e9d7c9dc51d7cff7de05626f
mewmicat <yachileetw@gmail.com>
2025-05-05 20:43:59 +0800

    Create hello_world_and_more.c

---
+++ week 1/hello_world_and_more.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello\nworld!\n");
+    printf("Hello \"C\" world!\n");
+    printf("Hello\\nworld!\n");
+    printf("H     H     A\n"
+           "H     H    A A\n"
+           "HHHHHHH   A   A\n"
+           "H     H  AAAAAAA\n"
+           "H     H  A     A\n");
+    return 0;
+}
